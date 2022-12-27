effac49746bc7f48d609ce974c2f17652052e133
DelTaZz93 <93768539+DelTaZz93@users.noreply.github.com>
2022-12-27 17:56:36 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,19 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'DelTaZz'
+description 'Un hud fait par DelTaZz pour DelTaZz Developpment'
+discord 'https://discord.gg/SCxttZrGw4'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua',
+	'config.lua'
+}
+
+files {
+	'html/ui.html',
+	'html/style.css',
+	'html/main.js',
+	'html/img/*.png'
+}
---
+++ html/main.js
@@ -0,0 +1,19 @@
+$(function () {
+	window.addEventListener('message', function(event) {
+		if (event.data.action == "setValue") {
+			setValue(event.data.key, event.data.value);
+		} else if (event.data.action == "updateStatus") {
+			updateStatus(event.data.status);
+		} else if (event.data.action == "toggle") {
+			if (event.data.show) {
+				$('#ui').show();
+			} else {
+				$('#ui').hide();
+			}
+		}
+	});
+});
+
+function setValue(key, value) {
+	$('#' + key + ' span').html(value);
+}
